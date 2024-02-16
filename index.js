b5097a0d2fc4f6f25c407caa2f84ea32c08f38b1
Kelthon <kelthonbalbino@gmail.com>
2024-02-16 10:26:42 -0300

    refact: files was formated using prettier

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const messageList = document.getElementById('view-messages');
+const messageList = document.getElementById("view-messages");
 const inputMessage = document.getElementById("send-message-input");
 const inputSendButton = document.getElementById("send-message-button");
 const inputForm = document.getElementById("send-message-form");
 
 function getCurrentTime() {
-    return (
-        new Date()
-            .toLocaleDateString("en-US", {
-                hour: "numeric",
-                minute: "numeric",
-                hour12: true,
-            })
-            .split(',')[1]
-            .trim()
-    );
+  return new Date()
+    .toLocaleDateString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    })
+    .split(",")[1]
+    .trim();
 }
 
 function addMessage() {
-    if(inputMessage.value) {
-        
-        messageList.innerHTML = `${messageList.innerHTML}
+  if (inputMessage.value) {
+    messageList.innerHTML = `${messageList.innerHTML}
         <li class="sended">
             <div>
                 <p>${inputMessage.value}</p>
@@ -29,13 +26,13 @@ function addMessage() {
             </div>
         </li>`;
 
-        inputMessage.value = "";
-    }
+    inputMessage.value = "";
+  }
 }
 
 inputSendButton.addEventListener("click", () => addMessage());
 
-inputForm.addEventListener("submit", event => {
-    event.preventDefault();
-    addMessage();
+inputForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addMessage();
 });
