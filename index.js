5724c7a998dbc65dbc562db56f6087964bb64916
Kelthon <kelthonbalbino@gmail.com>
2024-02-20 15:17:18 -0300

    refact: load messages was refactores in functions, fixed view-once-photos view

--- index.js
+++ index.js
@@ -2,6 +2,56 @@ const messageList = document.getElementById("view-messages");
 const inputMessage = document.getElementById("send-message-input");
 const inputSendButton = document.getElementById("send-message-button");
 const inputForm = document.getElementById("send-message-form");
+const preloadedMessages = [
+  {
+    type: "text-message",
+    content: "Hello.",
+    sendDate: "02/18/2024 08:00:42",
+    isSender: false,
+  },
+  {
+    type: "text-message",
+    content: "Hi 😊.",
+    sendDate: "02/18/2024 08:01:57",
+    isSender: true,
+  },
+  {
+    type: "text-message",
+    content: "John Here.",
+    sendDate: "02/19/2024 15:25:42",
+    isSender: false,
+  },
+  {
+    type: "text-message",
+    content: "Can you send me the your annotations?",
+    sendDate: "02/19/2024 15:27:57",
+    isSender: false,
+  },
+  {
+    type: "text-message",
+    content: "Oh hi Hi John.",
+    sendDate: "02/19/2024 15:34:02",
+    isSender: true,
+  },
+  {
+    type: "text-message",
+    content: "Sure I do.",
+    sendDate: "02/19/2024 15:34:11",
+    isSender: true,
+  },
+  {
+    type: "view-once-photo",
+    content: null,
+    sendDate: "02/19/2024 15:39:26",
+    isSender: true,
+  },
+  {
+    type: "view-once-photo",
+    content: null,
+    sendDate: "02/19/2024 15:46:12",
+    isSender: false,
+  },
+];
 
 function getCurrentTime(date) {
   currentTime = !!date ? new Date(date) : new Date();
@@ -20,6 +70,42 @@ function getDateMark(date) {
   return `<li class="info-message">
   <p><small>${date}</small></p>
 </li>`;
+}
+
+function getViewOnceButton() {
+  return `<p>
+      <i class="ri-reply-fill ri-xl"></i>
+      <button type="button">
+        <i class="ri-camera-fill"></i>
+        <span>photo</span>
+      </button>
+    </p>`;
+}
+
+function getTextMessage(content, isSender, sendDate) {
+  return `<li class="text-message ${isSender ? "sended" : "received"}">
+        <div>
+          ${content}
+          <footer>
+            <small class="message-status">${getCurrentTime(
+              new Date(sendDate)
+            )}</small>
+          </footer>
+        </div>
+      </li>`;
+}
+
+function getViewOncePhoto(content, isSender, sendDate) {
+  return `<li class="view-once-photo ${isSender ? "sended" : "received"}">
+        <div>
+          ${content}
+          <footer>
+            <small class="message-status">${getCurrentTime(
+              new Date(sendDate)
+            )}</small>
+          </footer>
+        </div>
+      </li>`;
 }
 
 function addMessage() {
@@ -50,94 +136,99 @@ function addMessage() {
   }
 }
 
-function loadMessages() {
-  const preloadedMessages = [
-    {
-      type: "text-message",
-      content: "Hello.",
-      sendDate: "02/18/2024 08:00:42",
-      isSender: false,
-    },
-    {
-      type: "text-message",
-      content: "Hi 😊.",
-      sendDate: "02/18/2024 08:01:57",
-      isSender: true,
-    },
-    {
-      type: "text-message",
-      content: "John Here.",
-      sendDate: "02/19/2024 15:25:42",
-      isSender: false,
-    },
-    {
-      type: "text-message",
-      content: "Can you send me the your annotations?",
-      sendDate: "02/19/2024 15:27:57",
-      isSender: false,
-    },
-    {
-      type: "text-message",
-      content: "Oh hi Hi John.",
-      sendDate: "02/19/2024 15:34:02",
-      isSender: true,
-    },
-    {
-      type: "text-message",
-      content: "Sure I do.",
-      sendDate: "02/19/2024 15:34:11",
-      isSender: true,
-    },
-    {
-      type: "view-once-photo",
-      content: null,
-      sendDate: "02/19/2024 15:39:26",
-      isSender: true,
-    },
-    {
-      type: "view-once-photo",
-      content: null,
-      sendDate: "02/19/2024 15:46:12",
-      isSender: false,
-    },
-  ];
-  
-  let loadedMessages = ""
+function concatMessagesContent(type, contentsList) {
+  let fixContent = (content) => `<p>${content}</p>`;
 
-  for (let msg of preloadedMessages) {
-    mainContent = msg.content
+  if (type === "view-once-photo") {
+    fixContent = (content) => getViewOnceButton();
+  }
 
-    if (msg.type !== "text-message") {
-      mainContent = `<i class="ri-reply-fill ri-xl"></i>
-      <button type="button">
-        <i class="ri-camera-fill"></i>
-        <span>photo</span>
-      </button>`;
+  if (Array.isArray(contentsList)) {
+    let concatenatedContent = "";
+
+    for (content of contentsList) {
+      concatenatedContent = `${concatenatedContent}
+    ${fixContent(content)}`;
     }
 
-    loadedMessages = `${loadedMessages}
-      <li class="${msg.type} ${msg.isSender ? "sended" : "received"}">
-        <div>
-          <p>
-            ${mainContent}
-          </p>
-          <footer>
-            <small class="message-status">${getCurrentTime(
-              new Date(msg.sendDate)
-            )}</small>
-          </footer>
-        </div>
-      </li>`;
-
-    messageList.innerHTML = loadedMessages;
+    return concatenatedContent;
   }
+
+  return `<p>${fixContent(message)}</p>`;
+}
+
+function groupMessages(messageList, groupFunction) {
+  let groups = [];
+  let array = [];
+  let lastType = "";
+  let lastSender = false;
+
+  for (message of messageList) {
+    if (lastType === "") {
+      lastType = message.type;
+      lastSender = message.isSender;
+    }
+
+    if (message.type === lastType && message.isSender === lastSender) {
+      array.push(message);
+      lastSender = message.isSender;
+    } else {
+      if (array.length > 0) {
+        groups.push({ type: lastType, messages: array });
+        array = [];
+      }
+
+      array.push(message);
+      lastType = message.type;
+      lastSender = message.isSender;
+    }
+  }
+
+  groups.push({ type: lastType, messages: array });
+
+  return groups;
+}
+
+function loadMessages() {
+  let messageGroups = groupMessages(preloadedMessages);
+  let loadedMessages = "";
+
+  for (group of messageGroups) {
+    let content = concatMessagesContent(
+      group.type,
+      group.messages.map((message) => {
+        return message.content;
+      })
+    );
+
+    console.log(content);
+
+    let isSender = group.messages[0].isSender;
+    let sendDate = group.messages[group.messages.length - 1].sendDate;
+
+    if (group.type === "text-message") {
+      loadedMessages = `${loadedMessages}${getTextMessage(
+        content,
+        isSender,
+        sendDate
+      )}`;
+    } else {
+      loadedMessages = `${loadedMessages}${getViewOncePhoto(
+        content,
+        isSender,
+        sendDate
+      )}`;
+    }
+  }
+
+  messageList.innerHTML = loadedMessages;
 }
 
 window.addEventListener("load", () => loadMessages());
 
 inputSendButton.addEventListener("click", () => addMessage());
 
-inputForm.addEventListener("submit", event => {
+inputForm.addEventListener("submit", (event) => {
   event.preventDefault();
   addMessage();
   messageList.scrollTop = messageList.scrollHeight;
